2ec08e1ede910fce3aa9d219fee0f606e04ba9a9
mag_daleneee <udokaineh04@gmail.com>
2023-02-16 23:39:07 +0100

    javaScript basics

---
+++ index.js
@@ -0,0 +1,17 @@
+function multiply (a , b) {
+    let result = a*b;
+    return result;
+}
+console.log(multiply(4,6));
+let firstname = "Udoka";
+lastname = "Ineh";
+skills = "designing , Coding , reading";
+stutern_track = "SWD cohort 1.4";
+favorite_color = "blue";
+year_of_my_country_independence = "october_1st_1960";
+console.log(firstname);
+console.log(lastname);
+console.log(skills);
+console.log(stutern_track);
+console.log(favorite_color);
+console.log(year_of_my_country_independence);
